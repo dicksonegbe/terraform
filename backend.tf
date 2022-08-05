1dd52368d259345604b749ee0cc35ba07b64f2b0
dicksonegbe <105952822+dicksonegbe@users.noreply.github.com>
2022-08-04 22:16:58 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@
     bucket = "terraform-jjtech-bucket"
     key    = "avinash/s3file-3.tfstate"
     region = "us-east-1"
-    profile = "jespo"
+    profile = "dickson"
 dynamodb_table= "firstprojecttable"
    }
  }
