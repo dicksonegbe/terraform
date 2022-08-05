5a8a8f6ea8442cc1541ee0bf654a97fc944586a4
dicksonegbe <105952822+dicksonegbe@users.noreply.github.com>
2022-08-04 22:12:55 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
  terraform {
    backend "s3" {
-    bucket = "code-sharing-bucket"
+    bucket = "terraform-jjtech-bucket"
     key    = "avinash/s3file-3.tfstate"
     region = "us-east-1"
     profile = "jespo"
